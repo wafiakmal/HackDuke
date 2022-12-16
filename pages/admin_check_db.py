569ab907cba3ea48d359401e66eacb8f974eedf3
Wafiakmal <104295829+wafiakmal@users.noreply.github.com>
2022-12-16 03:22:05 +0000

    3

--- pages/admin_check_db.py
+++ pages/admin_check_db.py
@@ -201,36 +201,48 @@ elif selection == "Customer Data":
         + "%",
     )
 
-    st.subheader("Current User Growth Rate and Average Users per Cup")
     query_growth_rate = "select month(join_date) as Month, count(customer_id) as count, (count(customer_id)-lag(count(customer_id), 1) over (order by month(join_date)))/lag(count(customer_id), 1) over (order by month(join_date)) as growth from cup_adventure.customers_db group by 1 order by 1"
     query_growth_rate = pd.read_sql(query_growth_rate, connection)
     growth_rate = (
         alt.Chart(query_growth_rate, title="Current User Growth Rate")
         .mark_line()
-        .encode(x="Month:N", y="growth:Q")
+        .encode(x="Month:N", y=alt.Y("growth:Q", title="User Growth", scale=alt.Scale(zero=False)))
     )
 
     query_customer_unique_users_per_cup = "SELECT month(transaction_date) as Month, count(customer_id)/count(distinct cup_id) as unique_users_per_cup FROM cup_adventure.transactions_log GROUP BY month(transaction_date)"
-    query_customer_unique_users_per_cup = pd.read_sql(
-        query_customer_unique_users_per_cup, connection
-    )
+    query_customer_unique_users_per_cup = pd.read_sql(query_customer_unique_users_per_cup, connection)
     customer_unique_users_per_cup = (
         alt.Chart(query_customer_unique_users_per_cup, title="Average Users per Cup")
         .mark_line()
-        .encode(x="Month:N", y="unique_users_per_cup:Q")
-    )
+        .encode(x="Month:N", y=alt.Y("unique_users_per_cup:Q", title="Unique Users per Cup", scale=alt.Scale(zero=False))))
 
-    # col4, col5 = st.columns(2)
     st.altair_chart(
         growth_rate.properties(width=300, height=300)
         | customer_unique_users_per_cup.properties(width=300, height=300),
         use_container_width=True,
     )
 
-    # col5 = st.altair_chart(
-    #     customer_unique_users_per_cup.properties(width=300, height=300),
-    #     use_container_width=True,
-    # )
+    query_customer_4 = "SELECT month(transaction_date) as Month, count(distinct vendor_id) as active_vendor FROM transactions_log WHERE transaction_status = 'Borrowed' GROUP BY month(transaction_date)"
+    df_customer_4 = pd.read_sql(query_customer_4, connection)
+    unique_cafe = (
+        alt.Chart(df_customer_4, title="Number of Active Cafe Distributing Our Cups")
+        .mark_line()
+        .encode(x="Month:N", y=alt.Y("active_vendor:Q", title="Active Vendors"))
+    )
+
+    query_customer_sold = "SELECT month(transaction_date) as Month, count(customer_id) as bought FROM transactions_log WHERE transaction_status = 'Bought' GROUP BY month(transaction_date)"
+    query_customer_sold = pd.read_sql(query_customer_sold, connection)
+    cup_sold = (
+        alt.Chart(query_customer_sold, title="Cups Sold")
+        .mark_line()
+        .encode(x="Month:N", y=alt.Y("bought:Q", title="Cups Sold"))
+    )
+
+    st.altair_chart(
+        unique_cafe.properties(width=300, height=300)
+        | cup_sold.properties(width=300, height=300),
+        use_container_width=True,
+    )
 
     customer_chart = (
         alt.Chart(df_customer_1)
