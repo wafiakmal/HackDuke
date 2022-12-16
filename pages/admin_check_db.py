5272cf8304a6f13e93b0581ed5ae6b15fcdea5c1
Wafiakmal <104295829+wafiakmal@users.noreply.github.com>
2022-12-16 01:39:28 +0000

    3

--- pages/admin_check_db.py
+++ pages/admin_check_db.py
@@ -193,14 +193,14 @@ elif selection == "Customer Data":
         "Total Deposit",
         "$" + (int(df_customer_3["Deposit"][df_customer_3["Deposit"] > 0].sum())),
         str(
-            (
+            int(
                 df_customer_3["Deposit"][df_customer_3["Deposit"] > 0].sum()
                 - df_customer_3["Deposit"][
                     df_customer_3["Month"]
                     != df_customer_3["Month"].sort_values(ascending=False).unique()[0]
                 ].sum()
             )
-            / (
+            / int(
                 df_customer_3["Deposit"][
                     df_customer_3["Month"]
                     != df_customer_3["Month"].sort_values(ascending=False).unique()[0]
